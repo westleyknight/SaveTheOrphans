f0d014d347b1b15a60635274b2eae32972cff81d
Dan Gates <dangates@CDOS-RUN.local>
2013-06-19 16:58:57 +0100

    Fixes issue where an orphan contained in an HTML tag would have it's HTML tag broken.

--- js/savetheorphans.js
+++ js/savetheorphans.js
@@ -1,22 +1,31 @@
 (function($){
 
-	$.fn.savetheorphans = function(options) {  
+	$.fn.savetheorphans = function(options) {
 
-		var defaults = {  
+		var defaults = {
 			orphansToSave: 1
-		};  
+		};
 
-		var options = $.extend(defaults, options); 
+		var options = $.extend(defaults, options);
 
 		return this.each(function() {
-			var obj = $(this);  
-			var str = obj.html();
+			var obj       = $(this),
+			    str       = obj.html(),
+			    htmlRegEx = /( )(<[^<]+?>.+<\/[^<]+>)/;
+
 			for (var i = 0; i < options.orphansToSave; i++) {
-				str = str.replace(/( )([^ ]*)$/,'&nbsp;$2');
+			    if (htmlRegEx.test(str)) {
+    			    // Because the last word is an HTML tag put the &nbsp; in before it else the tag will break
+    			    str = str.replace(htmlRegEx, '&nbsp;$2');
+
+			    } else {
+				    str = str.replace(/( )([^ ]*)$/, '&nbsp;$2');
+				}
 			};
+
 			obj.html(str);
 		});
 
 	};
 
-})(jQuery); 
+})(jQuery);
