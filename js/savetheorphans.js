6e5dba987aeee45c4cde2c7b512e3d3ad37f2068
Dan Gates <dangates@CDOS-RUN.local>
2013-06-19 17:13:34 +0100

    Tidy that commit up a bit...

--- js/savetheorphans.js
+++ js/savetheorphans.js
@@ -1,31 +1,28 @@
 (function($){
 
-	$.fn.savetheorphans = function(options) {
+	$.fn.savetheorphans = function(options) {  
 
-		var defaults = {
+		var defaults = {  
 			orphansToSave: 1
-		};
+		};  
 
-		var options = $.extend(defaults, options);
+		var options = $.extend(defaults, options); 
 
 		return this.each(function() {
-			var obj       = $(this),
-			    str       = obj.html(),
-			    htmlRegEx = /( )(<[^<]+?>.+<\/[^<]+>)/;
-
+			var obj = $(this);  
+			var str = obj.html();
+			var htmlRegEx = /( )(<[^<]+?>.+<\/[^<]+>)/;
 			for (var i = 0; i < options.orphansToSave; i++) {
-			    if (htmlRegEx.test(str)) {
-    			    // Because the last word is an HTML tag put the &nbsp; in before it else the tag will break
-    			    str = str.replace(htmlRegEx, '&nbsp;$2');
-
-			    } else {
-				    str = str.replace(/( )([^ ]*)$/, '&nbsp;$2');
+				if (htmlRegEx.test(str)) {
+					// Because the last word is an HTML tag put the &nbsp; in before it else the tag will break
+					str = str.replace(htmlRegEx, '&nbsp;$2');
+				} else {
+					str = str.replace(/( )([^ ]*)$/,'&nbsp;$2');
 				}
 			};
-
 			obj.html(str);
 		});
 
 	};
 
-})(jQuery);
+})(jQuery); 
